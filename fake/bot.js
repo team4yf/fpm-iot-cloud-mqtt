9ebd47ae9e64f4d0ec55c33a005873fb995cbd43
yfsoftcom <yfsoftcom@126.com>
2018-09-13 21:31:39 +0800

    test nb

--- fake/bot.js
+++ fake/bot.js
@@ -5,5 +5,5 @@ const data = Buffer.from([0x01,0x02,0x03,0x04])
 const payload = {topic: '#u1001/p1001/tcp', payload: data.toString('hex')}
 
 setInterval(function() {
-	client.publish('ding', JSON.stringify(payload), { qos: 1, retain: true});
+	client.publish('$s2d/nb/youren/push', Buffer.from('000000010000000200000002ffffffff01020304', 'hex'), { qos: 1, retain: true});
 },10000);
