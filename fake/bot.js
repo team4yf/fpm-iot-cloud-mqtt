c71818ffcec87b57d5ad078a661dfc385d761bc7
yfsoftcom <yfsoftcom@126.com>
2018-10-10 22:53:23 +0800

    change the bot

--- fake/bot.js
+++ fake/bot.js
@@ -4,9 +4,11 @@ const client = mqtt.connect('mqtt://localhost:1883', {
 	password: '123123123',
 });
 
-const data = Buffer.from([0x01,0x02,0x03,0x04])
-const payload = {topic: '#u1001/p1001/tcp', payload: data.toString('hex')}
-
 setInterval(function() {
 	client.publish('$s2d/nb/youren/push', Buffer.from('000000010000000200000002ffffffff01020304', 'hex'), { qos: 1, retain: true});
 },10000);
+
+client.subscribe('$s2d/nb/youren/push');
+client.on('message', (topic, payload) => {
+	console.log(topic, payload)
+})
